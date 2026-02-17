44019ff674797d0bd1b15ac37091a79bf5f39389
Noah Gitsham <me@noahgitsham.com>
2026-02-17 16:33:33 +0000

    Possibly nicer libc interaction for getting interfaces?

--- src/artnet/interface.rs
+++ src/artnet/interface.rs
@@ -1,4 +1,4 @@
-use std::{ffi::CStr, mem::MaybeUninit, net::{Ipv4Addr}};
+use std::{ffi::CStr, mem::MaybeUninit, net::Ipv4Addr};
 
 #[allow(nonstandard_style, unused)]
 pub enum IFF {
@@ -36,13 +36,14 @@ impl NetworkInterface {
         (self.flags & (flag as u32)) != 0
     }
 
-    pub fn get_interfaces_iter() -> NetworkInterfaceIter {
-        let mut ifaddrs = MaybeUninit::<*mut libc::ifaddrs>::uninit();
-        match unsafe { libc::getifaddrs(ifaddrs.as_mut_ptr()) } {
+    pub fn get_interfaces_iter<'a>() -> NetworkInterfaceIter<'a> {
+        let mut ifap = MaybeUninit::<*mut libc::ifaddrs>::uninit();
+        match unsafe { libc::getifaddrs(ifap.as_mut_ptr()) } {
             0 => {
+                let ifaddrs_init = unsafe { ifap.assume_init() }; // Safe getifaddrs ran
                 NetworkInterfaceIter {
-                    head: unsafe { ifaddrs.assume_init() },
-                    next: unsafe { ifaddrs.assume_init_read() },
+                    head: ifaddrs_init,
+                    next: unsafe { ifaddrs_init.as_ref() },
                 }
             }
             _ => todo!()
@@ -50,56 +51,61 @@ impl NetworkInterface {
     }
 }
 
-pub struct NetworkInterfaceIter {
-    head: *mut libc::ifaddrs,
-    next: *mut libc::ifaddrs,
+pub struct NetworkInterfaceIter<'a> {
+    head: *mut libc::ifaddrs, // For freeifaddrs on drop
+    next: Option<&'a libc::ifaddrs>,
 }
 
-pub fn addr_from_c_sock(sock: *mut libc::sockaddr) -> Option<Ipv4Addr> {
-    if sock.is_null() { return None }
-    let addr = unsafe {*sock};
-    match addr.sa_family as libc::c_int {
-        libc::AF_INET => {
-            // println!("{:?}, ", addr.sa_data as [u8; 14]);
-            let ip: [i8; 4] = addr.sa_data[2..6]
-                .try_into().unwrap();
-            let ip_u = ip.map(|i| i as u8);
-            Some(Ipv4Addr::from_octets(ip_u))
+pub fn addr_from_sockaddr(sockaddrp: *mut libc::sockaddr) -> Option<Ipv4Addr> {
+    let sock = unsafe { sockaddrp.as_ref() };
+    sock.map_or(None, |sock| {
+        match sock.sa_family as libc::c_int {
+            libc::AF_INET => {
+                let ip: [i8; 4] = sock.sa_data[2..6]
+                    .try_into().unwrap();
+                let ip_u = ip.map(|i| i as u8);
+                Some(Ipv4Addr::from_octets(ip_u))
+            }
+            _ => {
+                None
+            }
         }
-        // libc::AF_INET6 => {
-        //     println!("{:?}, ", addr.sa_data);
-        //     Some(IpAddr::V4(Ipv4Addr::new(0,0,0,0)))
-        // }
-        _ => {
-            None
-        }
+    })
+}
+
+fn read_next_interface(addr: &libc::ifaddrs) -> Option<&libc::ifaddrs> {
+    // Safe as longs as getifaddrs initialised ifaddrs
+    unsafe {addr.ifa_next.as_ref() }
+}
+
+fn unpack_interface(ifa: &libc::ifaddrs) -> NetworkInterface {
+    // Guaranteed to be initialised by getifaddrs
+    let name = unsafe { CStr::from_ptr(ifa.ifa_name) }.to_str()
+        .unwrap().to_owned();
+
+    NetworkInterface {
+        name,
+        addr: addr_from_sockaddr(ifa.ifa_addr),
+        _subnet_mask: addr_from_sockaddr(ifa.ifa_netmask),
+        flags: ifa.ifa_flags,
     }
 }
 
-impl Iterator for NetworkInterfaceIter {
+impl<'a> Iterator for NetworkInterfaceIter<'a> {
     type Item = NetworkInterface;
     fn next(&mut self) -> Option<Self::Item> {
 
-        if self.next.is_null() {
-            None
-        } else {
-            let ifa = unsafe {*self.next};
-            self.next = ifa.ifa_next;
-
-            let name = unsafe { CStr::from_ptr(ifa.ifa_name) }.to_str()
-                .unwrap().to_owned();
-
-            Some(NetworkInterface {
-                name,
-                addr: addr_from_c_sock(ifa.ifa_addr),
-                _subnet_mask: addr_from_c_sock(ifa.ifa_netmask),
-                flags: ifa.ifa_flags,
-            })
+        match self.next {
+            None => None,
+            Some(ifa) => {
+                self.next = read_next_interface(ifa);
+                Some(unpack_interface(ifa))
+            },
         }
     }
 }
 
-impl Drop for NetworkInterfaceIter {
+impl<'a> Drop for NetworkInterfaceIter<'a> {
     fn drop(&mut self) {
         unsafe{ libc::freeifaddrs(self.head) };
     }
